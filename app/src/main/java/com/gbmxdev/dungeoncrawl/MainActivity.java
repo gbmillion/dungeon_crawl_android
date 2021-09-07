2913aa42d341f6e61b7e61a0962670742e00cbed
gbmillion <gbmillion@bucs.fsw.edu>
2021-09-07 08:28:48 -0400

    imported java version directly into android package, just gonna use it like that instead of "porting" it.

--- app/src/main/java/com/gbmxdev/dungeoncrawl/MainActivity.java
+++ app/src/main/java/com/gbmxdev/dungeoncrawl/MainActivity.java
@@ -3,6 +3,9 @@ package com.gbmxdev.dungeoncrawl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user taps the Send button
+     */
+    public void sendMessage(View view) throws IOException {
+        int c1;
+        // Do something in response to button
+        System.out.println("Pressed button");
+        c1 = System.in.read();
+        System.out.println(c1);
     }
 }
